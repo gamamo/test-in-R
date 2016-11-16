3adc01d4dbf904902ad0b90faae64cabc3251ad2
Gabriel Moulatlet <gamamo@utu.fi>
2016-11-16 18:11:28 +0200

    appy with git and R v1

---
+++ test.R
@@ -0,0 +1,16 @@
+grep("[a-z]", letters)
+
+txt <- c("arm","foot","lefroo", "bafoobar")
+if(length(i <- grep("foo", txt)))
+  cat("'foo' appears at least once in\n\t", txt, "\n")
+i # 2 and 4
+txt[i]
+
+cat(paste(letters, 100* 1:26), fill = TRUE, labels = paste0("{", 1:10, "}:"))
+
+a <- seq(1:100)
+a
+a[-1]
+
+b <- c(seq(1:10))
+c <- c("i", "am","testing")
